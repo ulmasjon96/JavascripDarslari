db7e7af1d55ee41d04b82175de572d8f1883fc74
premiumelectro91 <premiumelectro91@xmail.ru>
2025-10-17 09:03:25 +0500

    19.5 - Dars qo'shildi

---
+++ JavaScript darsi Behzod Ustoz/19.5 - Dars praktika/js/main.js
@@ -0,0 +1,8 @@
+// Masala
+
+/**
+ * 1. sahifaning o'ng tomonidan reklama rasmini yuqoting
+ * 2. Filmning 'Kamediya' janrini 'Drama' ga o'zgartiring
+ * 3. Bosh fonda turgan rasmni 'bg.jpg' ga o'zgartiring. rasm img papkaga joylashgan
+ * 4.
+ */
